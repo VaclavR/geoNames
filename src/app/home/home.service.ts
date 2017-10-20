7085f5b2adecdbae64448b284169653139ccee43
VaclavR <vaclav.rysavy@tutanota.com>
2017-10-20 09:45:49 +0200

    experimenting with HttpClient

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -1,35 +1,55 @@
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
 import { Country } from '../Shared/country.model';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpRequest } from '@angular/common/http';
 
 @Injectable()
 export class HomeService {
   searchEmitted = new Subject<string>();
-  private url = 'http://api.geonames.org/countryInfoJSON?username=vencator';
+  private url = 'http://api.geonames.org/countryInfoJSON?';
   countries: Country[];
   currentList: Country[];
 
   constructor(private httpClient: HttpClient) { }
 
   getData(): Observable<Country[]> {
-    return this.httpClient.get<any[]>(this.url, {
-      observe: 'body',
-      responseType: 'json'
-    })
+    const req = new HttpRequest('GET', this.url, {
+      reportProgress: true,
+      params: new HttpParams().set('username', 'vencator')
+    });
+    return this.httpClient.request(req)
+    // return this.httpClient.get<any[]>(this.url, {
+    //   observe: 'body',
+    //   responseType: 'json',
+    //   params: new HttpParams().set('username', 'vencator')
+    // })
+      .do(data =>  {
+        if(data.type === 3) {
+          console.log(data['loaded']);
+        }
+      })
+      .debounceTime(100)
       .map(
         (data) => {
-          console.log(data);
-          this.countries = data['geonames'];
-          return data['geonames'];
+          this.countries = data['body'].geonames;
+          return data['body'].geonames;
         }
       );
   }
 
+
+
   getCountryDetail(countryCode: string): Observable<Country> {
-    return this.httpClient.get<any[]>(this.url + '&country=' + countryCode)
+    let params = new HttpParams();
+    params = params.append('username', 'vencator');
+    params = params.append('country', countryCode);
+    return this.httpClient.get<any[]>(this.url, {
+      params: params
+      // params: new HttpParams().set('username', 'vencator').set('country', countryCode)
+    })
       .map(
         (response) => {
           console.log(response);
